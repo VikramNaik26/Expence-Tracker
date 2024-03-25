a8c9dd04a7fd058d9221cfcf21c80fef9837be7d
Vikram Naik <26vikramnaik@gmail.com>
2024-03-25 16:44:24 +0530

    refactor: added a AppText widget for text input

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expence_tracker/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -9,153 +10,151 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 14.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.yellow.shade700,
-                      ),
-                    ),
-                    Icon(
-                      Icons.person,
-                      color: Colors.yellow.shade900,
-                    ),
-                  ],
-                ),
-                const Gap(8),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 14.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Stack(
+                    alignment: Alignment.center,
                     children: [
-                      Text(
-                        'Welcome!',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.outline,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w600,
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.yellow.shade700,
                         ),
                       ),
-                      Text(
-                        'Vikram Naik',
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onBackground,
+                      Icon(
+                        Icons.person,
+                        color: Colors.yellow.shade900,
+                      ),
+                    ],
+                  ),
+                  const Gap(8),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppText(
+                          text: 'Welcome!',
+                          textColor: Theme.of(context).colorScheme.outline,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        AppText(
+                          text: 'Vikram Naik',
+                          textColor: Theme.of(context).colorScheme.onBackground,
                           fontSize: 14,
                           fontWeight: FontWeight.bold,
                         ),
-                      ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.settings,
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(20.0),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.secondary,
+                      Theme.of(context).colorScheme.tertiary,
                     ],
+                    transform: const GradientRotation(pi / 4),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.settings,
-                  ),
-                ),
-              ],
-            ),
-            const Gap(20.0),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 2,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Theme.of(context).colorScheme.primary,
-                    Theme.of(context).colorScheme.secondary,
-                    Theme.of(context).colorScheme.tertiary,
+                  borderRadius: BorderRadius.circular(24.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade600,
+                      blurRadius: 2,
+                      offset: const Offset(3, 3),
+                    ),
                   ],
-                  transform: const GradientRotation(pi / 4),
                 ),
-                borderRadius: BorderRadius.circular(24.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade600,
-                    blurRadius: 2,
-                    offset: const Offset(3, 3),
-                  ),
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const _CardText(
-                    isTotalPrice: true,
-                  ),
-                  Gap(MediaQuery.of(context).size.width < 420 ? 20.0 : 30.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(6.0),
-                            decoration: const BoxDecoration(
-                              color: Colors.white30,
-                              shape: BoxShape.circle,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const _CardText(
+                      isTotalPrice: true,
+                    ),
+                    Gap(MediaQuery.of(context).size.width < 420 ? 20.0 : 30.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(6.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.arrow_upward,
+                                size: 12,
+                                color: Colors.greenAccent,
+                              ),
                             ),
-                            child: const Icon(
-                              Icons.arrow_upward,
-                              size: 12,
-                              color: Colors.greenAccent,
+                            const Gap(8.0),
+                            const _CardText(
+                              isTotalPrice: false,
+                              headingText: 'Income\n',
+                              headingFontSize: 14.0,
+                              headingFontWeight: FontWeight.w400,
+                              priceText: '₹ 2000.00',
+                              priceTextFontSize: 14.0,
+                              priceTextFontWeight: FontWeight.w600,
+                              lineHeight: 1.4,
                             ),
-                          ),
-                          const Gap(8.0),
-                          const _CardText(
-                            isTotalPrice: false,
-                            headingText: 'Income\n',
-                            headingFontSize: 14.0,
-                            headingFontWeight: FontWeight.w400,
-                            priceText: '₹ 2000.00',
-                            priceTextFontSize: 14.0,
-                            priceTextFontWeight: FontWeight.w600,
-                            lineHeight: 1.4,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(6.0),
-                            decoration: const BoxDecoration(
-                              color: Colors.white30,
-                              shape: BoxShape.circle,
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(6.0),
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.arrow_downward,
+                                size: 12,
+                                color: Colors.red,
+                              ),
                             ),
-                            child: const Icon(
-                              Icons.arrow_downward,
-                              size: 12,
-                              color: Colors.red,
+                            const Gap(8.0),
+                            const _CardText(
+                              isTotalPrice: false,
+                              headingText: 'Expenses\n',
+                              headingFontSize: 14.0,
+                              headingFontWeight: FontWeight.w400,
+                              priceText: '₹ 800.00',
+                              priceTextFontSize: 14.0,
+                              priceTextFontWeight: FontWeight.w600,
+                              lineHeight: 1.4,
                             ),
-                          ),
-                          const Gap(8.0),
-                          const _CardText(
-                            isTotalPrice: false,
-                            headingText: 'Expenses\n',
-                            headingFontSize: 14.0,
-                            headingFontWeight: FontWeight.w400,
-                            priceText: '₹ 800.00',
-                            priceTextFontSize: 14.0,
-                            priceTextFontWeight: FontWeight.w600,
-                            lineHeight: 1.4,
-                          ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            )
-          ],
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  const AppText({
+    super.key,
+    required this.text,
+    this.textColor,
+    this.fontSize,
+    this.fontWeight,
+  });
+
+  final String text;
+  final Color? textColor;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: textColor ?? Theme.of(context).colorScheme.onBackground,
+        fontSize: fontSize ?? 12,
+        fontWeight: fontWeight ?? FontWeight.normal,
+      ),
+    );
+  }
+}
