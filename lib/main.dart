6d6b72a6d7f6d04c2d5ba1a6d14e9d1b387c45a5
Vikram Naik <26vikramnaik@gmail.com>
2024-04-20 17:58:45 +0530

    Bloc setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:expence_tracker/app.dart';
+import 'package:expence_tracker/simple_bloc_observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
